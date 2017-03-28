3cec16342e799f71944027e2c8ef8cb813d980db
Unknown <tcf234@nyu.edu>
2017-03-28 00:34:04 -0400

    added randomness to pathmaker; started laptop model

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pathmaker : MonoBehaviour {
-
+    //used across pathmakers
     public static int tileCount = 0;
     public int tileCountReset;
 
@@ -11,35 +11,38 @@ public class Pathmaker : MonoBehaviour {
     public Transform floorPrefab;
     public Transform pathmakerSpherePrefab;
 
-    float speed;
+    int tileAllowance;
     
     void Awake()
     {
+        //allows user to press R without Unity thinking tileCount is maxed out
         tileCount = tileCountReset;
     }
 
     void Start()
     {
-        speed = Random.Range(3f, 7f); 
+        tileAllowance = Random.Range(30, 80);
     }
 
 	void Update () {
-        
-        if (counter < 50 && tileCount < 500)
+        //pathmakers die after placing 50; max of 500 tiles
+        if (counter < tileAllowance && tileCount < 500)
         {
             float rand = Random.Range(0.0f, 1.0f);
+            //low chance of turning
             if (rand < 0.1f)
             {
                 transform.Rotate(0f, 90f, 0f);
             } else if (rand < 0.2f)
             {
                 transform.Rotate(0f, -90f, 0f);
+            //somewhat rare to create another pathmaker
             } else if (rand > 0.955f)
             {
                 Instantiate(pathmakerSpherePrefab, transform.position, Quaternion.identity);
             }
             Instantiate(floorPrefab, transform.position, Quaternion.identity);
-            transform.Translate(0f, 0f, speed);
+            transform.Translate(0f, 0f, 5f);
             counter++;
             tileCount++;
             tileCountReset = tileCount;
