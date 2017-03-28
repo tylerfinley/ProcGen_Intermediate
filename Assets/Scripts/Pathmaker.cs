716c852ceeca299e1099ed8d1ac4d68649f1beb3
Unknown <tcf234@nyu.edu>
2017-03-28 00:02:40 -0400

    code for generating tiles

---
+++ Assets/Scripts/Pathmaker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathmaker : MonoBehaviour {
+
+    public static int tileCount = 0;
+    public int tileCountReset;
+
+    int counter = 0;
+    public Transform floorPrefab;
+    public Transform pathmakerSpherePrefab;
+
+    float speed;
+    
+    void Awake()
+    {
+        tileCount = tileCountReset;
+    }
+
+    void Start()
+    {
+        speed = Random.Range(3f, 7f); 
+    }
+
+	void Update () {
+        
+        if (counter < 50 && tileCount < 500)
+        {
+            float rand = Random.Range(0.0f, 1.0f);
+            if (rand < 0.1f)
+            {
+                transform.Rotate(0f, 90f, 0f);
+            } else if (rand < 0.2f)
+            {
+                transform.Rotate(0f, -90f, 0f);
+            } else if (rand > 0.955f)
+            {
+                Instantiate(pathmakerSpherePrefab, transform.position, Quaternion.identity);
+            }
+            Instantiate(floorPrefab, transform.position, Quaternion.identity);
+            transform.Translate(0f, 0f, speed);
+            counter++;
+            tileCount++;
+            tileCountReset = tileCount;
+        }
+        else {
+            Destroy(this.gameObject);
+        }
+        Debug.Log(tileCount);
+
+    }
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+    Pathmaker pathmakerScript;
+
+    void Start ()
+    {
+        pathmakerScript = GameObject.Find("Pathmaker").GetComponent<Pathmaker>();
+    }
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Main");
+            pathmakerScript.tileCountReset = 0;
+        }
+	}
+}
